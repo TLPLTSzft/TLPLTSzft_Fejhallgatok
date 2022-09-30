2000cfe0736dcab981610077368454ddfaa1ef0d
TLPLTSzft <tlpltszft@gmail.com>
2022-09-30 10:05:03 +0200

    Server oldali validalas

--- felvetel.php
+++ felvetel.php
@@ -9,6 +9,9 @@
 </head>
 
 <body>
+
+  <h1>Fejhallgatók felvétele</h1>
+
   <?php
   $szin_lista = [
     '' => "",
@@ -21,7 +24,7 @@
     'fekete' => "Fekete",
   ];
   ?>
-  <h1>Fejhallgatók felvétele</h1>
+
   <form action="felvetel.php" method="post" name="fejhallgato_felvetel" id="fejhallgato_felvetel">
     <label for="megnevezes_input">Megnevezés:</label>
     <input type="text" name="megnevezes" id="megnevezes_input" placeholder="Megnevezés">
@@ -43,13 +46,51 @@
       <?php endforeach ?>
     </select>
     <br>
-    <input type="checkbox" name="keszleten" id="keszleten_input">
-    <label for="keszleten_input">Készleten</label>
+    <label>Hangcsatorna:</label>
+    <input type="checkbox" name="hang_1" id="hang_1_input">
+    <label for="hang_1_input">1.0</label>
+    <input type="checkbox" name="hang_2" id="hang_2_input">
+    <label for="hang_2_input">2.0</label>
+    <input type="checkbox" name="hang_3" id="hang_3_input">
+    <label for="hang_3_input">5.1</label>
+    <input type="checkbox" name="hang_4" id="hang_4_input">
+    <label for="hang_4_input">7.1</label>
     <br>
     <label for="ar_input">Ár:</label>
     <input type="number" name="ar" id="ar_input" placeholder="Ár">
     <br>
+    <button type="reset">Kiürít</button>
+    <button type="submit">Felvétel</button>
   </form>
+
+  <?php if (isset($_POST) && !empty($_POST)) { ?>
+    <br> <?php echo  var_dump($_POST); ?> <br>
+  <?php
+    $hiba = "";
+    if (!isset($_POST['megnevezes']) || empty($_POST['megnevezes'])) {
+      $hiba .= "Megnevezés mező kitöltése kötelező!<br>";
+    }
+    if (!isset($_POST['csatlakozo']) || empty($_POST['csatlakozo'])) {
+      $hiba .= "Csatlakozó kiválasztása kötelező!<br>";
+    }
+    if (!isset($_POST['szin']) || empty($_POST['szin'])) {
+      $hiba .= "Szín kiválasztása kötelező!<br>";
+    } else if (!in_array($_POST['szin'], array_keys($szin_lista))) {
+      $hiba .= "Színt a legördülő listából kell választani!<br>";
+    }
+    if (!isset($_POST['hang_1']) && !isset($_POST['hang_2']) && !isset($_POST['hang_3']) && !isset($_POST['hang_4'])) {
+      $hiba .= "Hangcsatorna kiválasztása kötelező!<br>";
+    }
+    if (!isset($_POST['ar']) || empty($_POST['ar'])) {
+      $hiba .= "Ár megadása kötelező!<br>";
+    }
+    echo "<br>" . $hiba;
+  }
+  if ($hiba == "") {
+    echo "siker";
+  }
+  ?>
+
 </body>
 
 </html>
